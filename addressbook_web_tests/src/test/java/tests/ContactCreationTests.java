187dc0f9cf4c6c9d3b18024c1fa2f70e54fbcab0
ARoleder <nik_nastya96@mail.ru>
2023-11-22 13:52:21 +0300

    Цикл со счётчиком: многократное повторение похожих действий

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -31,4 +31,15 @@ public class ContactCreationTests extends TestBase {
 
         app.contacts().createContact(new ContactData().withFirstName("FirstName").withLastName("LastName").withMobilePhone("89990876655"));
     }
+
+    @Test
+    public void canCreateMultipleContacts() {
+        int n = 5;
+        int contactCount = app.contacts().getCount();
+        for (int i = 0; i < n; i++) {
+            app.contacts().createContact(new ContactData(randomString(i), randomString(5), "Ivan", "Ivashka", "Innovative", "Address", "5674", "89900032112", "858584333", "gsf@mail.ru", "hhh@gmail.com", "trew@yandex.ru", "secondary address", "home", "notes"));
+        }
+        int newContactCount = app.contacts().getCount();
+        Assertions.assertEquals(contactCount + n, newContactCount);
+    }
 }
