8f3ad04d8f10170454ded2d4d9f8f5426527806b
ARoleder <nik_nastya96@mail.ru>
2023-12-01 22:29:52 +0300

    Лекция 6.1. Драйвер базы данных. Получение данных из БД. Для создания группы и контакта

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -48,16 +48,18 @@ public class GroupCreationTests extends TestBase {
     @ParameterizedTest
     @MethodSource("groupProvider")
     public void canCreateMultipleGroups(GroupData group) {
-        var oldGroups = app.groups().getList();
+        var oldGroups = app.jdbc().getGroupList();
         app.groups().createGroup(group);
-        var newGroups = app.groups().getList();
+        var newGroups = app.jdbc().getGroupList();
         Comparator<GroupData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
         newGroups.sort(compareById);
+        var maxId =newGroups.get(newGroups.size() - 1).id();
         var expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+        expectedList.add(group.withId(maxId));
         expectedList.sort(compareById);
+        Assertions.assertEquals(newGroups, expectedList);
     }
 
     public static List<GroupData> negativeGroupProvider() {
