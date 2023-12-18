7e2688a5f61b6b14761b43c70b81f5616086a566
ARoleder <nik_nastya96@mail.ru>
2023-12-18 15:43:07 +0300

    Лекция 7.2. Функции-генераторы, или сапплаеры (Supplier)

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -14,6 +14,9 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Generator {
@@ -55,31 +58,47 @@ public class Generator {
         }
     }
 
+    private Object generateData (Supplier<Object> dataSupplier){
+       return Stream.generate(dataSupplier).limit(count).collect(Collectors.toList());
+    }
+
     private Object generateContacts() {
-        var result = new ArrayList<ContactData>();
-        for (int i = 0; i < count; i++) {
-            result.add(new ContactData()
-                    .withFirstName(CommonFunctions.randomString(i * 5))
-                    .withLastName(CommonFunctions.randomString(i * 5))
-                    .withMobilePhone("89990876655")
-                    .withAddress(CommonFunctions.randomString(i * 3))
-                    .withCompany(CommonFunctions.randomString(i * 3))
-                    .withEmail(CommonFunctions.randomString(i * 3)));
-            //.withPhoto(CommonFunctions.randomFile("src/test/resources/images")));
-        }
-        return result;
+        return  generateData(() -> new ContactData()
+                .withFirstName(CommonFunctions.randomString( 5))
+                .withLastName(CommonFunctions.randomString( 5))
+                .withMobilePhone("89990876655")
+                .withAddress(CommonFunctions.randomString(3))
+                .withCompany(CommonFunctions.randomString(3))
+                .withEmail(CommonFunctions.randomString( 3)));
+        //.withPhoto(CommonFunctions.randomFile("src/test/resources/images")));)
+//        var result = new ArrayList<ContactData>();
+//        for (int i = 0; i < count; i++) {
+//            result.add(new ContactData()
+//                    .withFirstName(CommonFunctions.randomString(i * 5))
+//                    .withLastName(CommonFunctions.randomString(i * 5))
+//                    .withMobilePhone("89990876655")
+//                    .withAddress(CommonFunctions.randomString(i * 3))
+//                    .withCompany(CommonFunctions.randomString(i * 3))
+//                    .withEmail(CommonFunctions.randomString(i * 3)));
+//            //.withPhoto(CommonFunctions.randomFile("src/test/resources/images")));
+//        }
+//        return result;
     }
 
     private Object generateGroups() {
-        var result = new ArrayList<GroupData>();
-        for (int i = 0; i < count; i++) {
-            result.add(new GroupData()
-                    .withName(CommonFunctions.randomString(i * 10))
-                    .withHeader(CommonFunctions.randomString(i * 10))
-                    .withFooter(CommonFunctions.randomString(i * 10)));
-        }
-        return result;
+        return  generateData(() -> new GroupData()
+                .withName(CommonFunctions.randomString(10))
+                .withHeader(CommonFunctions.randomString( 10))
+                .withFooter(CommonFunctions.randomString(10)));
     }
+//        var result = new ArrayList<GroupData>();
+//        for (int i = 0; i < count; i++) {
+//            result.add(new GroupData()
+//                    .withName(CommonFunctions.randomString(i * 10))
+//                    .withHeader(CommonFunctions.randomString(i * 10))
+//                    .withFooter(CommonFunctions.randomString(i * 10)));
+//        }
+//        return result;
 
     private void save(Object data) throws IOException {
         if ("json".equals(format)) {
