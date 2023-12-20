fe935b0d54cac72b824a219247361f2bd1564560
ARoleder <nik_nastya96@mail.ru>
2023-12-20 03:35:59 +0300

    Доработан ApplicationManager и добавлен класс TestBase

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -8,22 +8,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.Properties;
 
 public class ApplicationManager {
-    protected WebDriver driver;
+    private WebDriver driver;
+    private String string;
     private Properties properties;
 
     public void init(String browser, Properties properties) {
+        this.string = browser;
         this.properties = properties;
+    }
+
+    public WebDriver driver() {
         if (driver == null) {
-            if ("firefox".equals(browser)) {
+            if ("firefox".equals(string)) {
                 driver = new FirefoxDriver();
-            } else if ("chrome".equals(browser)) {
+            } else if ("chrome".equals(string)) {
                 driver = new ChromeDriver();
             } else {
-                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+                throw new IllegalArgumentException(String.format("Unknown browser %s", string));
             }
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1680, 921));
         }
+        return driver;
     }
 }
