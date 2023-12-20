3928295ae793dc62d0c6e449e40159ad69909ec9
ARoleder <nik_nastya96@mail.ru>
2023-12-20 13:17:52 +0300

    Лекция 8.3. Взаимодействие с сервером по протоколу HTTP

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 
 public class LoginTests extends TestBase {
 
-    @Test
-    void canLogin() {
+    @Test //через интерфейс
+    void canLoginUi() {
         app.session().login("administrator", "root");
         Assertions.assertTrue(app.session().isLoggedIn());
+    }
+
+    @Test //через http
+    void canLoginHttp() {
+        app.http().login("administrator", "root");
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 }
