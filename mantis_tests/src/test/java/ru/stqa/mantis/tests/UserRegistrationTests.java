2a36e7fa91327cc15a8b51c4f8c44c70fde27414
ARoleder <nik_nastya96@mail.ru>
2023-12-25 17:10:52 +0300

    Задание 19

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -3,9 +3,9 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.stqa.mantis.common.CommonFunctions;
+import ru.stqa.mantis.model.UserData;
 
 import java.time.Duration;
-import java.util.regex.Pattern;
 
 public class UserRegistrationTests extends TestBase {
 
@@ -33,11 +33,12 @@ public class UserRegistrationTests extends TestBase {
     void  canRegisterUserApi () {
         String username = CommonFunctions.randomString(8);
         var email = String.format("%s@localhost", username);
-        //создать пользователя на почтовом сервере (jameshelper)
+        //создание пользователя на почтовом сервере (jameshelper) с использованием REST API
         app.jamesApi().addUser(email, "password");
-        //заполняем форму создания и отправляем (браузер)
-        app.session().signup(username, email);
-        //ждем почту (mailhelper)
+        //регистрация нового пользователя в Mantis, используя REST API
+        app.rest().startRegistration(new UserData()
+                .withUserName(username)
+                .withEmail(email));
         //извлекаем ссылку из письма
         var messages = app.mail().receive(email, "password", Duration.ofSeconds(10));
         var text = messages.get(0).content();
@@ -45,7 +46,7 @@ public class UserRegistrationTests extends TestBase {
         //переходим по ссылке из письма и завершаем регистрацию (браузер)
         app.session().finishRegistration(url, username, "password");
         //проверяем, что пользователь может залогиниться(httpsessionhelper)
-        app.session().login(username, "password");
-        Assertions.assertTrue(app.session().isLoggedIn());
+        app.http().login(username, "password");
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 }
